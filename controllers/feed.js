094170bc08c2be5e3dcf584aa51bb90859e4223d
GAURAV <iamgaurav201@gmail.com>
2023-06-13 01:17:37 +0530

    new commit

--- controllers/feed.js
+++ controllers/feed.js
@@ -22,10 +22,13 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: 'Validation failed, entered data is incorrect.',
-      errors: errors.array()
-    });
+      const error = new Error('Validation failed, entered data is correct');
+      error.statusCode = 422;
+      throw error;
+    // return res.status(422).json({
+    //   message: 'Validation failed, entered data is incorrect.',
+    //   errors: errors.array()
+    // });
   }
   const title = req.body.title;
   const content = req.body.content;
