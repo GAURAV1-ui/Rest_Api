eb6e4f933f0868edd456ad06244b80879d6437a7
GAURAV <iamgaurav201@gmail.com>
2023-06-13 01:23:25 +0530

    new commit

--- app.js
+++ app.js
@@ -21,6 +21,13 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({message: message})
+});
+
 mongoose
   .connect(
     'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/messages?retryWrites=true'
